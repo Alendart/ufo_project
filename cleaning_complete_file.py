830c9a004d38d54f08a88ec713b451781740a393
Alendart <pawel.t.baraniak@gmail.com>
2024-12-09 22:18:32 +0100

    Scalono skryp w pytonie, i zweryfikowano jego poprawność

--- cleaning_complete_file.py
+++ cleaning_complete_file.py
@@ -2,10 +2,11 @@ import pandas as pd
 import datetime as dt
 import re
 import functools as fc
+import pycountry
 
 # Loading data to file
-countries = pd.read_csv(r'C:\UfoProject\data\countries.csv',sep=';')
-dataframe = pd.read_csv(r'C:\UfoProject\data\complete.csv',sep=',',on_bad_lines='warn')
+countries = pd.read_csv(r'C:\Repos\ufo_project\data\additional_files\countries.csv',sep=',')
+dataframe = pd.read_csv(r'C:\Repos\ufo_project\data\source_files\complete.csv',sep=',',on_bad_lines='warn')
 
 # countries dataframe preparing for merge
 countries['name'] = countries['name'].astype('str')
@@ -35,6 +36,23 @@ def country_check(a,string,value):
     else:
         return a
 
+#funkcja do wyszukania nazwy kraju po country_id przy użyciu ISC code
+def get_country_name(iso_code):
+    if isinstance(iso_code, str):  # Sprawdzamy, czy kod jest łańcuchem znaków
+        try:
+            iso_code = iso_code.upper()
+            country = pycountry.countries.get(alpha_2=iso_code)
+            return country.name if country else None
+        except KeyError:
+            return None
+    else:
+        return None  # Dla NaN i innych nieprawidłowych danych zwracamy None
+
+def get_country_code(name):
+    try:
+        return pycountry.countries.lookup(name).alpha_2  # Zwraca kod ISO alpha_2 (np. 'PL')
+    except LookupError:
+        return None  # Jeśli nie znajdzie kraju, zwraca None
 
 # Datetime column cleanups
 dataframe['date_changes'] = dataframe['datetime'].str.split(" ")
@@ -161,9 +179,99 @@ m_df = m_df.drop(columns=['shape_check'])
 
 # Renaming columns, preparing to save final files
 m_df = m_df.rename(columns={'datetime':'observation_datestamp','country':'country_id','shape':'shape_type','duration (seconds)':'duration_in_sec','duration (hours/min)':'duration_string','state':'state_id','date posted':'date_posted'})
-m_df.to_csv(r'data/complete_cleaned.csv',sep=';')
-m_df.to_parquet(r'data/complete_cleaned.parquet')
 
+# Saving of first version of files
+# m_df.to_csv(r'data/complete_cleaned.csv',sep=';')
+# m_df.to_parquet(r'data/complete_cleaned.parquet')
 
+m_df["observation_datestamp"] = pd.to_datetime(m_df["observation_datestamp"])
+m_df['date']=m_df['observation_datestamp'].dt.date
 
+# adding year column
+m_df['date']=pd.to_datetime(m_df['date'],format="%Y-%m-%d")
+m_df['year'] = m_df['date'].dt.year
+m_df.drop(columns="observation_datestamp",inplace=True)
 
+m_df['state_id']=m_df['state_id'].str.upper()
+m_df['country_id']=m_df['country_id'].str.upper()
+m_df['city']=m_df['city'].str.title()
+
+#usuwanie nawiasow wraz z zawartoscia
+m_df['city'] = m_df['city'].str.replace(r'\(.*?\)', '', regex=True)
+
+#usuwanie nawiasów otwierających wraz z zawartoscia
+m_df['city'] = m_df['city'].str.replace(r'\(.*?', '', regex=True)
+
+#ręczna poprawa powyższych danych
+m_df.loc[8194,'country_id']='CA'
+m_df.loc[5789,'city']='Unadilla'
+m_df.loc[8194,'city']='Ft. Resolution'
+m_df.loc[40609,'city']='Tel Aviv'
+m_df.loc[40609,'country_id']='IL'
+m_df.loc[52860,'city']='London'
+m_df.loc[52860,'country_id']='GB'  
+m_df.loc[45133,'city']='Iowa City'
+m_df.loc[54713, 'city']='Phuket'
+m_df.loc[54713, 'country_id']='TH'
+m_df.loc[60128, 'city']=''
+m_df.loc[60128, 'country_id']='AFG'
+m_df.loc[75757,'city']=''   
+m_df.loc[78575,'city']=''
+m_df.loc[80683,'city']='Midwest'
+m_df.loc[80683,'country_id']='US'
+
+#usuwanie nawiasow zamykających + spacji przed
+m_df['city'] = m_df['city'].str.replace(r' \)', '', regex=True)
+
+#usuwanie nawiasow zamykających bez spacji
+m_df['city'] = m_df['city'].str.replace(r'\)', '', regex=True)
+
+m_df['city'] = m_df['city'].str.strip().str.replace(r'\s+', ' ', regex=True)
+m_df['city'] = m_df['city'].str.replace(r'\bNas\b', '', regex=True)
+
+#nowa kolumna country_name, która korzysta z funkcji get_country_name
+m_df['country_name'] = m_df['country_id'].apply(get_country_name)
+
+#gdy country_name jest puste, dodajemy info z city
+m_df['country_name'] = m_df['country_name'].fillna(m_df['city'])
+
+m_df['id'] = m_df.index + 1
+
+new_order = ['id','date','year','city','state_id','country_id','country_name','shape_type','duration_in_sec','duration_string','comments','date_posted','latitude','longitude']
+m_df = m_df[new_order]
+
+#usunięcie niewidzialnych znaków z country_name
+m_df['country_name'] = m_df['country_name'].str.strip().str.replace(r'\s+', ' ', regex=True)
+
+#usuwanie znaków znaków innych niż Literowe
+m_df= m_df[~m_df['city'].str.contains(r'[^a-zA-Z\s]', na=False)]
+
+#usuwanie znaków zapytania
+m_df= m_df[~m_df['city'].str.contains(r'\?', na=False)]
+
+# Uzupełnianie brakujących wartości w kolumnie country_id
+m_df['country_id'] = m_df.apply(
+    lambda row: get_country_code(row['country_name']) if pd.isna(row['country_id']) else row['country_id'],
+    axis=1)
+
+#usuwanie pozycji gdzie country_id nie zostało uzupełnione
+m_df= m_df.dropna(subset=['country_id'])
+
+reg_df=pd.read_csv(r"C:\Repos\ufo_project\data\final_file\regions.csv",
+                sep=",",
+                header=0
+                  )
+
+df_merged=m_df.merge(right=reg_df,how='left',left_on='country_id',right_on='alpha-2')
+
+df_merged.drop(columns="country-code",inplace=True)
+df_merged.drop(columns="iso_3166-2",inplace=True)
+df_merged.drop(columns="intermediate-region",inplace=True)
+df_merged.drop(columns="region-code",inplace=True)
+df_merged.drop(columns="sub-region-code",inplace=True)
+df_merged.drop(columns="intermediate-region-code",inplace=True)
+
+cleared_data = df_merged
+
+cleared_data.to_csv(r"C:\Repos\ufo_project\data\cleared_data_py.csv",index=False)
+cleared_data.to_parquet(r"C:\Repos\ufo_project\data\cleared_data_py.parquet",index=False)
